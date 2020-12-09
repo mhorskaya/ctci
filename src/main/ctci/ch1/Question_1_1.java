662fe569c8583321f02f14d1908cae9775e749da
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-12-09 14:36:06 +0300

    1.1 - is unique

--- src/main/ctci/ch1/Question_1_1.java
+++ src/main/ctci/ch1/Question_1_1.java
@@ -5,10 +5,24 @@ import java.util.HashSet;
 public class Question_1_1 {
     public static boolean isUnique(String str) {
         HashSet<Character> set = new HashSet<>();
-        char[] charArray = str.toCharArray();
-        for (char c : charArray) {
+        for (char c : str.toCharArray()) {
+            if (set.contains(c)) {
+                return false;
+            }
             set.add(c);
         }
-        return set.size() == str.length();
+        return true;
+    }
+
+    public static boolean isUnique2(String str) {
+        int map = 0;
+        for (char c : str.toCharArray()) {
+            int bit = 1 << c - 'a';
+            if ((map & bit) > 0) {
+                return false;
+            }
+            map |= bit;
+        }
+        return true;
     }
 }
--- src/test/ctci/ch1/Test_1_1.java
+++ src/test/ctci/ch1/Test_1_1.java
@@ -5,8 +5,14 @@ import org.junit.Test;
 
 public class Test_1_1 {
     @Test
-    public void test1() {
+    public void isUniqueTest() {
         Assert.assertTrue(Question_1_1.isUnique("khan"));
         Assert.assertFalse(Question_1_1.isUnique("hakan"));
+    }
+
+    @Test
+    public void isUnique2Test() {
+        Assert.assertTrue(Question_1_1.isUnique2("khan"));
+        Assert.assertFalse(Question_1_1.isUnique2("hakan"));
     }
 }
